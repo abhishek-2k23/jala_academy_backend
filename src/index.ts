b15b518aff9f8e123153c320ffa907cf26977011
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-28 23:21:30 +0530

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import dbConnect from './configuration/Db';
 import router from './routes/route';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+// Allow requests from your frontend origin
+app.use(cors({
+  origin: '*', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 //connect db
 dbConnect();
