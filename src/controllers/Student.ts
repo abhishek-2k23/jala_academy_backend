3a23800fe2338e99511be57d0d6558885b2a0c11
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-31 10:59:02 +0530

    removed searchquery controller

--- src/controllers/Student.ts
+++ src/controllers/Student.ts
@@ -92,56 +92,6 @@ export const createStudent = async (req, res) => {
   }
 }
 
-export const searchStudent = async (req, res) => {
-  try {
-    const { searchInput } = req.params // Extract search searchInput from request searchInput params
-
-    if (!searchInput) {
-      return res.status(400).json({
-        status: false,
-        message: "Search searchInput is required",
-      })
-    }
-
-    let searchCriteria
-
-    // Determine the type of data (email, phoneNumber, or name)
-    if (searchInput.includes("@")) {
-      // If the searchInput contains '@', assume it's an email
-      searchCriteria = { email: searchInput }
-    } else if (/^\d+$/.test(searchInput)) {
-      // If the searchInput is numeric, assume it's a phone number
-      searchCriteria = { phoneNumber: searchInput }
-    } else {
-      // Otherwise, assume it's a name
-      searchCriteria = { name: { $regex: searchInput, $options: "i" } } // Case-insensitive search
-    }
-
-    // Search the database
-    const student = await Student.findOne(searchCriteria).lean()
-
-    if (!student) {
-      return res.status(404).json({
-        status: false,
-        message: "No student found matching the searchInput",
-      })
-    }
-
-    // Return the found student
-    return res.status(200).json({
-      status: true,
-      message: "Student found",
-      student,
-    })
-  } catch (error) {
-    console.error("Error searching for student:", error.message)
-    return res.status(500).json({
-      status: false,
-      message: "Server error",
-    })
-  }
-}
-
 export const searchStudentWithNumber = async (req, res) => {
   try {
     const { phoneNumber } = req.params
--- src/routes/Route.ts
+++ src/routes/Route.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import { login, register } from "../controllers/Auth";
-import {createStudent, getStudentData, searchStudent, searchStudentWithNumber, updateStudent} from '../controllers/Student'
+import {createStudent, getStudentData, searchStudentWithNumber, updateStudent} from '../controllers/Student'
 const router = express.Router();
 
 router.post('/user/register', register);
 router.post('/user/login', login);
 router.post('/student/create', createStudent);
 router.put('/student/update/:id', updateStudent);
-router.get('/student/search/:searchInput', searchStudent);
 router.get('/student/searchWithPhoneNumber/:phoneNumber', searchStudentWithNumber);
 router.get('/student/getStudentData', getStudentData);
 export default router;
